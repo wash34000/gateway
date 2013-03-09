2b81f272588aec55a8f0a9a4ab394613767c010d
Frederick Ryckbosch <frederick.ryckbosch@gmail.com>
2013-03-09 17:55:05 +0100

    Master baudrate set to 115200. Turn off stats2 led.

--- src/openmotics_service.py
+++ src/openmotics_service.py
@@ -69,7 +69,7 @@ def main():
     passthrough_serial_port = config.get('OpenMotics', 'passthrough_serial')
     power_serial_port = config.get('OpenMotics', 'power_serial')
     
-    controller_serial = Serial(controller_serial_port, 19200)
+    controller_serial = Serial(controller_serial_port, 115200)
     passthrough_serial = Serial(passthrough_serial_port, 19200)
     power_serial = RS485(Serial(power_serial_port, 115200))
     
