539f020ca872abe5ef29ed561ab0288737ec164c
Kenneth Henderick <kenneth@ketronic.be>
2016-05-19 19:52:51 +0200

    Enhance power API

--- src/power/power_api.py
+++ src/power/power_api.py
@@ -17,7 +17,9 @@ ADDRESS_MODE = 1
 POWER_API_8_PORTS = 8
 POWER_API_12_PORTS = 12
 
-NUM_PORTS = { POWER_API_8_PORTS : 8, POWER_API_12_PORTS : 12}
+NUM_PORTS = {POWER_API_8_PORTS: 8,
+             POWER_API_12_PORTS: 12}
+
 
 def get_general_status(version):
     """ Get the general status of a power module.
@@ -30,6 +32,7 @@ def get_general_status(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def get_time_on(version):
     """ Get the time the power module is on (in s)
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -38,6 +41,7 @@ def get_time_on(version):
         return PowerCommand('G', 'TON', '', 'L')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_feed_status(version):
     """ Get the feed status of the power module (12x 0=low or 1=high)
@@ -50,6 +54,7 @@ def get_feed_status(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def get_feed_counter(version):
     """ Get the feed counter of the power module
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -58,6 +63,7 @@ def get_feed_counter(version):
         return PowerCommand('G', 'FCO', '', 'H')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_voltage(version):
     """ Get the voltage of a power module (in V)
@@ -70,6 +76,7 @@ def get_voltage(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def get_frequency(version):
     """ Get the frequency of a power module (in Hz)
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -80,6 +87,7 @@ def get_frequency(version):
         return PowerCommand('G', 'FRE', '', '12f')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_current(version):
     """ Get the current of a power module (12x in A)
@@ -92,6 +100,7 @@ def get_current(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def get_power(version):
     """ Get the power of a power module (12x in W)
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -102,6 +111,7 @@ def get_power(version):
         return PowerCommand('G', 'POW', '', '12f')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_normal_energy(version):
     """ Get the total energy measured by the power module (12x in Wh)
@@ -114,6 +124,7 @@ def get_normal_energy(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def get_day_energy(version):
     """ Get the energy measured during the day by the power module (12x in Wh)
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -124,6 +135,7 @@ def get_day_energy(version):
         return PowerCommand('G', 'EDA', '', '12L')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_night_energy(version):
     """ Get the energy measured during the night by the power module (12x in Wh)
@@ -136,6 +148,7 @@ def get_night_energy(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def set_day_night(version):
     """ Set the power module in night (0) or day (1) mode.
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -146,6 +159,7 @@ def set_day_night(version):
         return PowerCommand('S', 'SDN', '12b', '')
     else:
         raise ValueError("Unknown power api version")
+
 
 def get_sensor_types(version):
     """ Get the sensor types used on the power modules (8x sensor type).
@@ -158,6 +172,7 @@ def get_sensor_types(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def set_sensor_types(version):
     """ Set the sensor types used on the power modules (8x sensor type).
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -168,6 +183,7 @@ def set_sensor_types(version):
         raise ValueError("Setting sensor types is not applicable for the 12 port modules.")
     else:
         raise ValueError("Unknown power api version")
+
 
 def set_current_clamp_factor(version):
     """ Sets the current clamp factor.
@@ -180,6 +196,7 @@ def set_current_clamp_factor(version):
     else:
         raise ValueError('Unknown power api version')
 
+
 def set_current_inverse(version):
     """ Sets the current inverse.
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -191,11 +208,63 @@ def set_current_inverse(version):
     else:
         raise ValueError('Unknown power api version')
 
-## Below are the address mode functions.
+
+# Below are the more advanced function (12p module only)
+
+def get_voltage_sample_time(version):
+    """ Gets a voltage sample (time - oscilloscope view)
+    :param version: power api version
+    """
+    if version == POWER_API_12_PORTS:
+        return PowerCommand('G', 'VST', '2b', '50f')
+    elif version == POWER_API_8_PORTS:
+        raise ValueError("Getting a voltage sample (time) is not applicable for the 8 port modules.")
+    else:
+        raise ValueError("Unknown power api version")
+
+
+def get_current_sample_time(version):
+    """ Gets a current sample (time - oscilloscope view)
+    :param version: power api version
+    """
+    if version == POWER_API_12_PORTS:
+        return PowerCommand('G', 'CST', '2b', '50f')
+    elif version == POWER_API_8_PORTS:
+        raise ValueError("Getting a current sample (time) is not applicable for the 8 port modules.")
+    else:
+        raise ValueError("Unknown power api version")
+
+
+def get_voltage_sample_frequency(version):
+    """ Gets a voltage sample (frequency)
+    :param version: power api version
+    """
+    if version == POWER_API_12_PORTS:
+        return PowerCommand('G', 'VSF', '2b', '40f')
+    elif version == POWER_API_8_PORTS:
+        raise ValueError("Getting a voltage sample (frequency) is not applicable for the 8 port modules.")
+    else:
+        raise ValueError("Unknown power api version")
+
+
+def get_current_sample_frequency(version):
+    """ Gets a current sample (frequency)
+    :param version: power api version
+    """
+    if version == POWER_API_12_PORTS:
+        return PowerCommand('G', 'CSF', '2b', '40f')
+    elif version == POWER_API_8_PORTS:
+        raise ValueError("Getting a current sample (frequency) is not applicable for the 8 port modules.")
+    else:
+        raise ValueError("Unknown power api version")
+
+
+# Below are the address mode functions.
 
 def set_addressmode():
     """ Set the address mode of the power module, 1 = address mode, 0 = normal mode """
     return PowerCommand('S', 'AGT', 'b', '')
+
 
 def want_an_address(version):
     """ The Want An Address command, send by the power modules in address mode. """
@@ -206,16 +275,23 @@ def want_an_address(version):
     else:
         raise ValueError('Unknown power api version')
 
+
 def set_address():
     """ Reply on want_an_address, setting a new address for the power module. """
     return PowerCommand('S', 'SAD', 'b', '')
+
 
 def set_voltage():
     """ Calibrate the voltage of the power module. """
     return PowerCommand('S', 'SVO', 'f', '')
 
 
-## Below are the function to reset the kwh counters
+def set_current():
+    """ Calibrate the voltage of the power module. """
+    return PowerCommand('S', 'SCU', 'f', '')
+
+
+# Below are the function to reset the kwh counters
 
 def reset_normal_energy(version):
     """ Reset the total energy measured by the power module.
@@ -240,6 +316,7 @@ def reset_day_energy(version):
     else:
         raise ValueError("Unknown power api version")
 
+
 def reset_night_energy(version):
     """ Reset the energy measured during the night by the power module.
     :param version: power api version (POWER_API_8_PORTS or POWER_API_12_PORTS).
@@ -252,27 +329,32 @@ def reset_night_energy(version):
         raise ValueError("Unknown power api version")
 
 
-## Below are the bootloader functions
+# Below are the bootloader functions
 
 def bootloader_goto():
     """ Go to bootloader and wait for a number of seconds (b parameter) """
     return PowerCommand('S', 'BGT', 'B', '')
 
+
 def bootloader_read_id():
     """ Get the device id """
     return PowerCommand('G', 'BRI', '', '8B')
+
 
 def bootloader_write_code():
     """ Write code """
     return PowerCommand('S', 'BWC', '195B', '')
 
+
 def bootloader_write_configuration():
     """ Write configuration """
     return PowerCommand('S', 'BWF', '24B', '')
 
+
 def bootloader_jump_application():
     """ Go from bootloader to applications """
     return PowerCommand('S', 'BJA', '', '')
+
 
 def get_version():
     """ Get the current version of the power module firmware """
