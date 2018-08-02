cc8449efb29d4c81988b4f10b6e6d09eec281a8c
Kenneth Henderick <kenneth@ketronic.be>
2018-08-02 14:21:03 +0200

    More efficient led control

--- src/led_service.py
+++ src/led_service.py
@@ -59,8 +59,10 @@ class LOGGER(object):
 
 
 def is_beagle_bone_black():
-    """ Use the total memory size to determine whether the code runs on a Beaglebone
-    or on a Beaglebone Black. """
+    """
+    Use the total memory size to determine whether the code runs on a Beaglebone
+    or on a Beaglebone Black.
+    """
     with open('/proc/meminfo', 'r') as meminfo:
         mem_total = meminfo.readline()
     return "510716 kB" in mem_total
@@ -74,10 +76,12 @@ def is_button_pressed(gpio_pin):
 
 
 def detect_button(gpio_1, gpio_2):
-    """ Detect which gpio pin is connected the input button. If the button is not connected, the
+    """
+    Detect which gpio pin is connected the input button. If the button is not connected, the
     pin will look as if the button is pressed. So if there is one input that is not pressed, it
     must be connected to the button. If both pins look pressed, it defaults to the first given
-    gpio. """
+    gpio.
+    """
     first_pressed = is_button_pressed(gpio_1)
     second_pressed = is_button_pressed(gpio_2)
 
@@ -90,14 +94,17 @@ def detect_button(gpio_1, gpio_2):
 
 
 class StatusObject(dbus.service.Object):
-    """ The StatusObject contains the methods exposed over dbus, the serial and network activity
-    checkers and the 'authorized' button checker. """
+    """
+    The StatusObject contains the methods exposed over dbus, the serial and network activity
+    checkers and the 'authorized' button checker.
+    """
 
     def __init__(self, bus, path, i2c_device, i2c_address, input_button):
         dbus.service.Object.__init__(self, bus, path)
         self._i2c_device = i2c_device
         self._i2c_address = i2c_address
         self._input_button = input_button
+        self._input_button_pressed_since = None
 
         self._network_enabled = False
         self._network_activity = False
@@ -105,6 +112,7 @@ class StatusObject(dbus.service.Object):
 
         self._serial_activity = {4: False, 5: False}
         self._enabled_leds = {}
+        self._enabled_gpio = {}
         self._previous_gpio = {}
         self._last_code = 0
 
@@ -112,8 +120,7 @@ class StatusObject(dbus.service.Object):
         self._authorized_timeout = 0
         self._authorized_index = 0
 
-        self._led_thread = None
-        self._button_thread = None
+        self._check_states_thread = None
 
         self._is_bbb = is_beagle_bone_black()
 
@@ -121,12 +128,9 @@ class StatusObject(dbus.service.Object):
 
     def start(self):
         """ Start the leds and buttons thread. """
-        self._led_thread = Thread(target=self._drive_leds)
-        self._led_thread.daemon = True
-        self._led_thread.start()
-        self._button_thread = Thread(target=self._check_button)
-        self._button_thread.daemon = True
-        self._button_thread.start()
+        self._check_states_thread = Thread(target=self._check_states)
+        self._check_states_thread.daemon = True
+        self._check_states_thread.start()
 
     @dbus.service.method("com.openmotics.status", in_signature='', out_signature='')
     def clear_leds(self):
@@ -139,13 +143,11 @@ class StatusObject(dbus.service.Object):
     def set_led(self, led_name, enable):
         """ Set the state of a LED, enabled means LED on in this context. """
         self._enabled_leds[led_name] = bool(enable)
-        self._write_leds()
 
     @dbus.service.method("com.openmotics.status", in_signature='s', out_signature='')
     def toggle_led(self, led_name):
         """ Toggle the state of a LED. """
         self._enabled_leds[led_name] = not self._enabled_leds[led_name]
-        self._write_leds()
 
     @dbus.service.method("com.openmotics.status", in_signature='i', out_signature='')
     def serial_activity(self, port):
@@ -154,132 +156,138 @@ class StatusObject(dbus.service.Object):
 
     @dbus.service.method("com.openmotics.status", in_signature='', out_signature='b')
     def in_authorized_mode(self):
-        """ Returns whether the gateway is in authorized mode. Authorized mode is enabled when the
-        button on the top panel is pushed and lasts for 60 seconds. """
+        """
+        Returns whether the gateway is in authorized mode. Authorized mode is enabled when the
+        button on the top panel is pushed and lasts for 60 seconds.
+        """
         return self._authorized_mode
 
-    def _get_i2c_code(self):
-        """ Generates the i2c code for the LEDs. """
-        code = 0
-        for led in CODES:
-            if self._enabled_leds[led] is True:
-                code |= CODES[led]
-
-        if self._authorized_mode:  # Light all leds in authorized mode
-            code |= AUTH_CODE
-
-        return (~ code) & 255
+    def set_gpio(self, gpio, on):
+        """ Sets GPIO on/off """
+        self._enabled_gpio[gpio] = on
 
     def _write_leds(self):
         """ Set the LEDs using the current status. """
         try:
-            # Update leds
-            new_code = self._get_i2c_code()
-            if new_code != self._last_code:
-                self._last_code = new_code
+            # Get i2c code
+            code = 0
+            for led in CODES:
+                if self._enabled_leds[led] is True:
+                    code |= CODES[led]
+            if self._authorized_mode:
+                # Light all leds in authorized mode
+                code |= AUTH_CODE
+            code = (~ code) & 255
+
+            # Push code if needed
+            if code != self._last_code:
+                self._last_code = code
                 with open(self._i2c_device, 'r+', 1) as i2c:
                     fcntl.ioctl(i2c, IOCTL_I2C_SLAVE, self._i2c_address)
-                    i2c.write(chr(new_code))
+                    i2c.write(chr(code))
         except Exception as exception:
             LOGGER.log('Error while writing to i2c: {0}'.format(exception))
 
-    def _drive_leds(self):
-        while True:
-            try:
-                self.network()
-                self.serial()
-            except Exception as exception:
-                LOGGER.log('Error while driving leds: {0}'.format(exception))
-            time.sleep(0.25)
-
-    def set_gpio(self, gpio, on):
-        """ Sets GPIO on/off """
-        try:
+    def _write_gpio(self):
+        """ Sets the GPIO using the current status. """
+        for gpio in self._enabled_gpio:
+            on = self._enabled_gpio[gpio]
             if self._previous_gpio.get(gpio) != on:
-                with open('/sys/class/gpio/gpio{0}/value'.format(gpio), 'w') as fh_s:
-                    fh_s.write('1' if on else '0')
                 self._previous_gpio[gpio] = on
-        except IOError:
-            pass  # The GPIO doesn't exist or is read only
+                try:
+                    with open('/sys/class/gpio/gpio{0}/value'.format(gpio), 'w') as fh_s:
+                        fh_s.write('1' if on else '0')
+                except IOError:
+                    pass  # The GPIO doesn't exist or is read only
 
-    def serial(self):
-        """ Function that toggles the UART LEDs in case there was activity on the port.
-        This function registers itself with the gobject creating a loop that runs every 100 ms.
-        """
-        for uart in [4, 5]:
-            uart_name = 'uart{0}'.format(uart)
-            if self._serial_activity[uart]:
-                self.toggle_led(uart_name)
-            else:
-                self.set_led(uart_name, False)
-            self._serial_activity[uart] = False
-
-    def network(self):
-        """ Function that set the LEDs on the ethernet port using the statistics from /proc/net.
-        This function registers itself with the gobject creating a loop that runs every 100 ms.
-        """
-        with open('/sys/class/net/eth0/carrier', 'r') as fh_up:
-            line = fh_up.read()
-        self._network_enabled = int(line) == 1
-
-        with open('/proc/net/dev', 'r') as fh_stat:
-            for line in fh_stat.readlines():
-                if 'eth0' in line:
-                    received, transmitted = 0, 0
-                    parts = line.split()
-                    if len(parts) == 17:
-                        received = parts[1]
-                        transmitted = parts[9]
-                    elif len(parts) == 16:
-                        (_, received) = tuple(parts[0].split(':'))
-                        transmitted = parts[8]
-                    new_bytes = received + transmitted
-                    if self._network_bytes != new_bytes:
-                        self._network_bytes = new_bytes
-                        self._network_activity = True
-                    else:
-                        self._network_activity = False
-
-        if not self._is_bbb:
-            self.set_gpio(ETH_LEFT_BB, self._network_enabled)
-            self.set_gpio(ETH_RIGHT_BB, self._network_activity)
-        else:
-            self.set_gpio(ETH_STATUS_BBB, not self._network_enabled)
-            if self._network_activity:
-                self.toggle_led('alive')
-            else:
-                self.set_led('alive', False)
-
-    def _check_button(self):
-        pressed_since = None
+    def _check_states(self):
+        """ Checks various states of the system (network) """
         while True:
             try:
-                if self._authorized_mode:
-                    if time.time() > self._authorized_timeout:
-                        self._authorized_mode = False
-                        self.set_gpio(HOME, True)
-                    else:
-                        self.set_gpio(HOME, AUTH_LOOP[self._authorized_index])
-                        self._authorized_index = (self._authorized_index + 1) % len(AUTH_LOOP)
-                        # Still in authorized mode...
-                else:
-                    if is_button_pressed(self._input_button):
-                        if pressed_since is None:
-                            pressed_since = time.time()
-                        if time.time() > pressed_since + 5:
-                            self._authorized_mode = True
-                            self._authorized_timeout = time.time() + 60
-                            pressed_since = None
-                    else:
-                        pressed_since = None
+                with open('/sys/class/net/eth0/carrier', 'r') as fh_up:
+                    line = fh_up.read()
+                self._network_enabled = int(line) == 1
+
+                with open('/proc/net/dev', 'r') as fh_stat:
+                    for line in fh_stat.readlines():
+                        if 'eth0' in line:
+                            received, transmitted = 0, 0
+                            parts = line.split()
+                            if len(parts) == 17:
+                                received = parts[1]
+                                transmitted = parts[9]
+                            elif len(parts) == 16:
+                                (_, received) = tuple(parts[0].split(':'))
+                                transmitted = parts[8]
+                            new_bytes = received + transmitted
+                            if self._network_bytes != new_bytes:
+                                self._network_bytes = new_bytes
+                                self._network_activity = True
+                            else:
+                                self._network_activity = False
             except Exception as exception:
-                LOGGER.log('Error while checking button: {0}'.format(exception))
+                LOGGER.log('Error while checking states: {0}'.format(exception))
             time.sleep(0.5)
+
+    def drive_leds(self):
+        """ This drives different leds (status, alive and serial) """
+        try:
+            # Calculate network led/gpio states
+            if not self._is_bbb:
+                self.set_gpio(ETH_LEFT_BB, self._network_enabled)
+                self.set_gpio(ETH_RIGHT_BB, self._network_activity)
+            else:
+                self.set_gpio(ETH_STATUS_BBB, not self._network_enabled)
+                if self._network_activity:
+                    self.toggle_led('alive')
+                else:
+                    self.set_led('alive', False)
+            # Calculate serial led states
+            for uart in [4, 5]:
+                uart_name = 'uart{0}'.format(uart)
+                if self._serial_activity[uart]:
+                    self.toggle_led(uart_name)
+                else:
+                    self.set_led(uart_name, False)
+                self._serial_activity[uart] = False
+            # Update all leds & gpio
+            self._write_leds()
+            self._write_gpio()
+        except Exception as exception:
+            LOGGER.log('Error while driving leds: {0}'.format(exception))
+        gobject.timeout_add(250, self.drive_leds)
+
+    def check_button(self):
+        """ Handles input button presses """
+        try:
+            if self._authorized_mode:
+                if time.time() > self._authorized_timeout:
+                    self._authorized_mode = False
+                    self.set_gpio(HOME, True)
+                else:
+                    self.set_gpio(HOME, AUTH_LOOP[self._authorized_index])
+                    self._authorized_index = (self._authorized_index + 1) % len(AUTH_LOOP)
+                    # Still in authorized mode...
+            else:
+                if is_button_pressed(self._input_button):
+                    if self._input_button_pressed_since is None:
+                        self._input_button_pressed_since = time.time()
+                    if time.time() > self._input_button_pressed_since + 5:
+                        self._authorized_mode = True
+                        self._authorized_timeout = time.time() + 60
+                        self._input_button_pressed_since = None
+                else:
+                    self._input_button_pressed_since = None
+        except Exception as exception:
+            LOGGER.log('Error while checking button: {0}'.format(exception))
+        gobject.timeout_add(250, self.check_button)
 
 
 def main():
-    """ The main function runs a loop that waits for dbus calls, drives the leds and reads the
-    switch. """
+    """
+    The main function runs a loop that waits for dbus calls, drives the leds and reads the
+    switch.
+    """
     try:
         config = ConfigParser()
         config.read(constants.get_config_file())
@@ -302,6 +310,10 @@ def main():
 
         LOGGER.log("Running led service.")
         mainloop = gobject.MainLoop()
+
+        gobject.timeout_add(250, status.drive_leds)
+        gobject.timeout_add(250, status.check_button)
+
         mainloop.run()
     except Exception as exception:
         LOGGER.log('Error starting led service: {0}'.format(exception))
