9067e72476a0e94284a903cf2c4434303fd52744
Kenneth Henderick <kenneth@ketronic.be>
2018-10-16 18:29:46 +0200

    Make sure the passthrough isn't used when not configured

--- src/master/master_communicator.py
+++ src/master/master_communicator.py
@@ -52,7 +52,7 @@ class MasterCommunicator(object):
         :param verbose: Print all serial communication to stdout.
         :type verbose: boolean.
         :param watchdog_period: The number of seconds between two watchdog checks.
-        :type watchdog_perdiod: integer.
+        :type watchdog_period: integer.
         :param watchdog_callback: The action to call if the watchdog detects a communication \
         problem.
         :type watchdog_callback: function without arguments.
@@ -77,6 +77,7 @@ class MasterCommunicator(object):
 
         self.__consumers = []
 
+        self.__passthrough_enabled = False
         self.__passthrough_mode = False
         self.__passthrough_timeout = passthrough_timeout
         self.__passthrough_queue = Queue()
@@ -119,6 +120,9 @@ class MasterCommunicator(object):
         self.__stop = False
         self.__read_thread.start()
         self.__watchdog_thread.start()
+
+    def set_passthrough_enabled(self, enabled):
+        self.__passthrough_enabled = enabled
 
     def get_bytes_written(self):
         """ Get the number of bytes written to the Master. """
@@ -348,7 +352,7 @@ class MasterCommunicator(object):
             """ Callback for when consumer is done. ReadState does not access parent directly. """
             if isinstance(consumer, Consumer):
                 self.__consumers.remove(consumer)
-            elif isinstance(consumer, BackgroundConsumer) and consumer.send_to_passthrough:
+            elif isinstance(consumer, BackgroundConsumer) and consumer.send_to_passthrough and self.__passthrough_enabled:
                 self.__passthrough_queue.put(consumer.last_cmd_data)
 
         class ReadState(object):
@@ -413,7 +417,7 @@ class MasterCommunicator(object):
                 # No else here: data might not be empty when current_consumer is done
                 if read_state.should_find_consumer():
                     start_bytes = self.__get_start_bytes()
-                    leftovers = "" # for unconsumed bytes; these will go to the passthrough.
+                    leftovers = ""  # for unconsumed bytes; these will go to the passthrough.
 
                     while len(data) > 0:
                         if data[0] in start_bytes:
@@ -442,7 +446,8 @@ class MasterCommunicator(object):
 
                     if len(leftovers) > 0:
                         if not self.__maintenance_mode:
-                            self.__passthrough_queue.put(leftovers)
+                            if self.__passthrough_enabled:
+                                self.__passthrough_queue.put(leftovers)
                         else:
                             self.__maintenance_queue.put(leftovers)
 
